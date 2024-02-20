8eae7495288eec2b9553e03920a96f80c7765a28
valentino andric <lynxdutch@gmail.com>
2024-02-20 13:50:33 +0100

    migrated from pages to app router. still need to fix skills.tsx

--- components/nav/index.tsx
+++ components/nav/index.tsx
@@ -1,42 +1,52 @@
-import styles from './index.module.scss';
+import styles from "./index.module.scss";
 
-import Image from 'next/image';
-import Pfp from '../../src/media/pfp.jpg';
+import Image from "next/image";
+import Pfp from "../../src/media/pfp.jpg";
 
-import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 
-const Nav = () => {
-    return (
-        <>
-            <div className={styles.nav}>
-                <div className={styles.container}>
-                    <div className={styles.left}>
-                        <h1>Valentino Andric</h1>
-                    </div>
+export default function Nav() {
+  return (
+    <>
+      <div className={styles.nav}>
+        <div className={styles.container}>
+          <div className={styles.left}>
+            <h1>Valentino Andric</h1>
+          </div>
 
-                    <div className={styles.middle}>
-                        <ul>
-                            <li><a href="#about">About</a></li>
-                            <li><a href="#skills">Skills</a></li>
-                            <li><a href="#footer">Contact</a></li>
-                        </ul>
-                    </div>
+          <div className={styles.middle}>
+            <ul>
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="#skills">Skills</a>
+              </li>
+              <li>
+                <a href="#footer">Contact</a>
+              </li>
+            </ul>
+          </div>
 
-                    <div className={styles.right}>
-                        
-                        <a href="https://www.linkedin.com/in/valentino-andric-04a4bb267/" target='_blank'><AiFillLinkedin className={styles.socialmedia}/></a>
-                        <a href="https://github.com/vnandric" target='_blank'><AiFillGithub className={styles.socialmedia}/></a>
-                    </div>
-                </div>                
-            </div>
+          <div className={styles.right}>
+            <a
+              href="https://www.linkedin.com/in/valentino-andric-04a4bb267/"
+              target="_blank"
+            >
+              <AiFillLinkedin className={styles.socialmedia} />
+            </a>
+            <a href="https://github.com/vnandric" target="_blank">
+              <AiFillGithub className={styles.socialmedia} />
+            </a>
+          </div>
+        </div>
+      </div>
 
-            {/* background image and I make it darker with an overlay */}
-            <div className={styles.img_overlay}>
-                <Image src={Pfp} alt="pfp" className={styles.pfp}/>
-                <div className={styles.overlay}></div>
-            </div>
-        </>
-    )
+      {/* background image and I make it darker with an overlay */}
+      <div className={styles.img_overlay}>
+        <Image src={Pfp} alt="pfp" className={styles.pfp} />
+        <div className={styles.overlay}></div>
+      </div>
+    </>
+  );
 }
-
-export default Nav;
--- components/skills/index.tsx
+++ components/skills/index.tsx
@@ -1,86 +1,134 @@
 /* eslint-disable react/no-unescaped-entities */
-import styles from './index.module.scss';
+"use client";
 
-import React from 'react';
+import styles from "./index.module.scss";
 
-import Image from 'next/image';
-import Js from '../../src/media/js-code.png';
+import React from "react";
 
-import ProgressBar from '@ramonak/react-progress-bar';
+import Image from "next/image";
+import Js from "../../src/media/js-code.png";
 
-const Skills = () => {
-    return (
-        <>
-            <div className={styles.container} id='skills'>                
-                <div className={styles.main}>
-                    <div className={styles.table}>     
-                        <h1>Skills</h1>          
-                        <table>
-                            <tr>
-                                <td>HTML</td>
-                                <td>
-                                    <ProgressBar completed={80} width='450px' bgColor='cyan' labelColor='black' baseBgColor='#252525'/>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>CSS</td>
-                                <td>
-                                    <ProgressBar completed={80} width='450px' bgColor='cyan' labelColor='black' baseBgColor='#252525'/>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>JavaScript</td>
-                                <td>
-                                    <ProgressBar completed={60} width='450px' bgColor='cyan' labelColor='black' baseBgColor='#252525'/>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>PHP</td>
-                                <td>
-                                    <ProgressBar completed={70} width='450px' bgColor='cyan' labelColor='black' baseBgColor='#252525'/>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>React</td>
-                                <td>
-                                    <ProgressBar completed={20} width='450px' bgColor='cyan' labelColor='black' baseBgColor='#252525'/>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Node.js</td>
-                                <td>
-                                    <ProgressBar completed={20} width='450px' bgColor='cyan' labelColor='black' baseBgColor='#252525'/>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Next.js</td>
-                                <td>
-                                    <ProgressBar completed={20} width='450px' bgColor='cyan' labelColor='black' baseBgColor='#252525'/>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>C#</td>
-                                <td>
-                                    <ProgressBar completed={40} width='450px' bgColor='cyan' labelColor='black' baseBgColor='#252525'/>
-                                </td>
-                            </tr>
-                        </table>
-                    </div>
+import ProgressBar from "@ramonak/react-progress-bar";
 
-                    <Image src={Js} alt='javascript code' className={styles.image}/>
+export default function Skills() {
+  return (
+    <>
+      <div className={styles.container} id="skills">
+        <div className={styles.main}>
+          <div className={styles.table}>
+            <h1>Skills</h1>
+            <table>
+              <tr>
+                <td>HTML</td>
+                <td>
+                  <ProgressBar
+                    completed={80}
+                    width="450px"
+                    bgColor="cyan"
+                    labelColor="black"
+                    baseBgColor="#252525"
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>CSS</td>
+                <td>
+                  <ProgressBar
+                    completed={80}
+                    width="450px"
+                    bgColor="cyan"
+                    labelColor="black"
+                    baseBgColor="#252525"
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>JavaScript</td>
+                <td>
+                  <ProgressBar
+                    completed={60}
+                    width="450px"
+                    bgColor="cyan"
+                    labelColor="black"
+                    baseBgColor="#252525"
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>PHP</td>
+                <td>
+                  <ProgressBar
+                    completed={70}
+                    width="450px"
+                    bgColor="cyan"
+                    labelColor="black"
+                    baseBgColor="#252525"
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>React</td>
+                <td>
+                  <ProgressBar
+                    completed={20}
+                    width="450px"
+                    bgColor="cyan"
+                    labelColor="black"
+                    baseBgColor="#252525"
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>Node.js</td>
+                <td>
+                  <ProgressBar
+                    completed={20}
+                    width="450px"
+                    bgColor="cyan"
+                    labelColor="black"
+                    baseBgColor="#252525"
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>Next.js</td>
+                <td>
+                  <ProgressBar
+                    completed={20}
+                    width="450px"
+                    bgColor="cyan"
+                    labelColor="black"
+                    baseBgColor="#252525"
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>C#</td>
+                <td>
+                  <ProgressBar
+                    completed={40}
+                    width="450px"
+                    bgColor="cyan"
+                    labelColor="black"
+                    baseBgColor="#252525"
+                  />
+                </td>
+              </tr>
+            </table>
+          </div>
 
-                </div>
+          <Image src={Js} alt="javascript code" className={styles.image} />
+        </div>
 
-                <div className={styles.text}>
-                    <p> 
-                        I am improving and expanding my skills, and it's unfortunate that I cannot showcase a project online. I can only display 
-                        a few projects on my GitHub, where you can also find the portfolio website that I created myself called portfolio2.0.
-                    </p>
-                </div>
-                
-            </div>
-        </>
-    )
+        <div className={styles.text}>
+          <p>
+            I am improving and expanding my skills, and it's unfortunate that I
+            cannot showcase a project online. I can only display a few projects
+            on my GitHub, where you can also find the portfolio website that I
+            created myself called portfolio2.0.
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
-
-export default Skills;
